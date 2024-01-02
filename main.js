9090c3fd12bff7f27d3d5910ad579096c93d99f1
Gadzooksgaming <102859034+Gadzooksgaming@users.noreply.github.com>
2024-01-02 16:04:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+var images = ["back1.png", "back2.png", "back 4.png", "back 5.png"];
+var randomImage = images[Math.floor(Math.random() * images.length)];
+document.body.style.backgroundImage = "url('" + randomImage + "')";
+
+leftWristX=0;
+leftWristy=0;
+rightWristX=0;
+rightWristY=0;
+
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(){
+    if(results.lenght>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("LeftWristX"+leftWristX+"leftWristY"+leftWristY);
+        
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristy=results[0].pose.rightWrist.y;
+        console.log("rightWristX"+rightWristX+"rightWristY"+rightWristY);
+    }
+}
+
+function draw(){
+    image(video,0,0,600,500);
+
+}
+
+song="";
+
+function preload(){
+    song=loadSound("music.mp3");
+
+
+}
+
+
+function play()
+{  
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+  }
+
